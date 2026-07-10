66ec83e005fe759d2113cfb56de61259a319f7bb
Pranav Chopade <pranav-4797@users.noreply.github.com>
2026-07-10 18:16:17 +0530

    Fix build: Apply compileSdk override only on plugin subprojects

--- scribble_arena/android/build.gradle.kts
+++ scribble_arena/android/build.gradle.kts
@@ -19,9 +19,13 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 subprojects {
-    plugins.withId("com.android.library") {
-        configure<com.android.build.gradle.BaseExtension> {
-            compileSdkVersion(36)
+    if (project.name != "app") {
+        afterEvaluate {
+            if (project.hasProperty("android")) {
+                project.extensions.configure<com.android.build.gradle.BaseExtension> {
+                    compileSdkVersion(36)
+                }
+            }
         }
     }
 }

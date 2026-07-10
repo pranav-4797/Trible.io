7aea3d6b9755908b0b14f876c5a2cae62942daea
Pranav Chopade <pranav-4797@users.noreply.github.com>
2026-07-10 19:22:52 +0530

    Fix package mismatch: Relocate MainActivity.kt to com.example.trible package directory

--- scribble_arena/android/app/src/main/kotlin/com/example/scribble_arena/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.scribble_arena
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ scribble_arena/android/app/src/main/kotlin/com/example/trible/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.trible
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()

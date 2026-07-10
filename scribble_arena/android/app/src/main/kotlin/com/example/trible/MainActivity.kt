f2ebb6daaa50d16e9b0115aa176acde67c3f6c0d
Pranav Chopade <pranav-4797@users.noreply.github.com>
2026-07-10 20:00:38 +0530

    Configure Firebase Web Options, set up Firebase Hosting configs, and deprecate Android directories

--- scribble_arena/android/app/src/main/kotlin/com/example/trible/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.trible
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- scribble_arena/lib/firebase_options.dart
+++ scribble_arena/lib/firebase_options.dart
@@ -17,10 +17,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -58,5 +55,15 @@ class DefaultFirebaseOptions {
     messagingSenderId: '633661851326',
     projectId: 'trible-d2ff2',
     storageBucket: 'trible-d2ff2.firebasestorage.app',
+  );
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyAn86oNWo2tOpcBnVTtytdvKfXrc0vqH7o',
+    appId: '1:633661851326:web:6b1036a161b524c0ef80ef',
+    messagingSenderId: '633661851326',
+    projectId: 'trible-d2ff2',
+    authDomain: 'trible-d2ff2.firebaseapp.com',
+    storageBucket: 'trible-d2ff2.firebasestorage.app',
+    measurementId: 'G-0N5SHSWG53',
   );
 }
